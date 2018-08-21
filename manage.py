19f8d51f3663f0dffc39a4203a806f6627e1ba9b
k <gavrilovks@gmail.com>
2018-08-20 22:52:34 -0500

    add manager

--- manage.py
+++ manage.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from app import create_app
 
-app = Flask(__name__)
+from app.database import db
+from flask_script import Manager, Shell
+from flask_migrate import MigrateCommand, Migrate
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app = create_app('C:/Projects/flask-easypost/config_dev.py')
+manager = Manager(app)
+migrate = Migrate(app, db)
 
+
+def make_shell_context():
+    return dict(app=app, db=db, models=models)
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
+    #app.run()
+
