a7d40c60031d5c0d20628d3b8824b9a03e2f467b
k <gavrilovks@gmail.com>
2018-08-24 23:34:31 -0500

    created form of Class

--- app/schdl_class/forms.py
+++ app/schdl_class/forms.py
@@ -1,11 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, RadioField
+from wtforms import StringField, SubmitField, SelectField, RadioField, BooleanField
 from wtforms.validators import DataRequired, Optional
 
 
 class ClassForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    description = StringField('Description', validators=[DataRequired()])
-    url = StringField('URL', validators=[DataRequired()])
+    school_id = SelectField('School', coerce=int, validators=[Optional()])
+    teacher_id = SelectField('Teacher', coerce=int, validators=[Optional()])
+    subject_id = SelectField('Subject', coerce=int, validators=[Optional()])
+    #note = StringField('Description', validators=[DataRequired()])
+    current = BooleanField('Current Class')
     submit = SubmitField('Update')
 
