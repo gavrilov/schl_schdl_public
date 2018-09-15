b66bc75e4bc218d0cec291f14d54770c113ceff1
k <gavrilovks@gmail.com>
2018-09-15 15:21:24 -0500

    Merge all contacts

--- app/models.py
+++ app/models.py
@@ -22,26 +22,12 @@ class Schdl_Class(db.Model):
 class School(db.Model):
     __tablename__ = "schools"
     id = db.Column('id', db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     name = db.Column('name', db.Unicode(2048))
     director_name = db.Column('director_name', db.Unicode(2048))
     note = db.Column('note', db.Unicode(2048))
     current = db.Column('current', db.Boolean())
     classes = db.relationship('Schdl_Class', backref='school', lazy='dynamic')
-    contacts = db.relationship('SchoolContacts', backref='school', lazy='dynamic')
-
-
-class SchoolContacts(db.Model):
-    __tablename__ = "schoolcontacts"
-    id = db.Column('id', db.Integer, autoincrement=True, primary_key=True)
-    school_id = db.Column(db.Integer, db.ForeignKey('schools.id'))
-    note = db.Column('note', db.Unicode(2048))
-    email = db.Column('email', db.Unicode(2048))
-    phone = db.Column('phone', db.Unicode(2048), index=True)
-    address1 = db.Column('address1', db.Unicode(2048))
-    address2 = db.Column('address2', db.Unicode(2048))
-    city = db.Column('city', db.Unicode(2048))
-    state = db.Column('state', db.Unicode(2048))
-    zip = db.Column('zip', db.Unicode(2048))
 
 
 class Subject(db.Model):
@@ -55,26 +41,10 @@ class Subject(db.Model):
 class Teacher(db.Model):
     __tablename__ = "teachers"
     id = db.Column('id', db.Integer, autoincrement=True, primary_key=True)
-    first_name = db.Column('first_name', db.Unicode(2048))
-    last_name = db.Column('last_name', db.Unicode(2048))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     current = db.Column('current', db.Boolean())
     classes = db.relationship('Schdl_Class', backref='teacher', lazy='dynamic')
     events = db.relationship('Event', backref='teacher', lazy='dynamic')
-    contacts = db.relationship('TeacherContacts', backref='teacher', lazy='dynamic')
-
-
-class TeacherContacts(db.Model):
-    __tablename__ = "teachercontacts"
-    id = db.Column('id', db.Integer, autoincrement=True, primary_key=True)
-    teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'))
-    note = db.Column('note', db.Unicode(2048))
-    email = db.Column('email', db.Unicode(2048))
-    phone = db.Column('phone', db.Unicode(2048), index=True)
-    address1 = db.Column('address1', db.Unicode(2048))
-    address2 = db.Column('address2', db.Unicode(2048))
-    city = db.Column('city', db.Unicode(2048))
-    state = db.Column('state', db.Unicode(2048))
-    zip = db.Column('zip', db.Unicode(2048))
 
 
 class Event(db.Model):
