0319b523a18c7c1a85ea1797a632940cda08634d
k <gavrilovks@gmail.com>
2018-09-21 21:15:10 -0500

    admin

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Optional
 
 
@@ -10,12 +10,3 @@ class UserForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
-class SchoolForm(FlaskForm):
-    name = StringField('School Name', validators=[DataRequired()])
-    director_name = StringField('Director Name', validators=[Optional()])
-    note = StringField('Note', validators=[Optional()])
-    current = BooleanField('Current School')
-    # password = StringField('Password', validators=[DataRequired()])
-    # email = StringField('Email', validators=[DataRequired()])
-    # easypost_api_key = StringField('EasyPost API Key', validators=[DataRequired()])
-    submit = SubmitField('Submit')
--- app/admin/views.py
+++ app/admin/views.py
@@ -1,9 +1,9 @@
-from flask import render_template, Blueprint, current_app, redirect, url_for, flash, request
+from flask import render_template, Blueprint, current_app, redirect, url_for, flash
 from flask_security import current_user, roles_required
 
 from app import db
-from app.admin.forms import UserForm, SchoolForm
-from app.models import School, Teacher, Subject, Schdl_Class, Event, User, Student
+from app.admin.forms import UserForm
+from app.models import Teacher, Subject, Schdl_Class, Event, User, Student
 
 admin = Blueprint('admin', __name__, template_folder='templates')
 
@@ -13,75 +13,6 @@ admin = Blueprint('admin', __name__, template_folder='templates')
 def main():
     current_app.logger.info("Admin {} has been signed in to admin console".format(current_user.email))
     return render_template('admin/dashboard.html')
-
-
-@admin.route('/school', methods=['GET', 'POST'])
-@roles_required('admin')
-def school_list():
-    if request.method == 'POST':
-        # TODO csrf_token to ajax request
-        data = request.form['action']
-        print(data)
-        data = request.form.getlist('schools')
-        for school_id in data:
-            school = School.query.filter_by(id=school_id).first()
-            print(school.name)
-    schools = School.query.filter_by(current=True).all()
-    return render_template('admin/school_list.html', schools=schools, current_schools_only=True)
-
-
-@admin.route('/school/all', methods=['GET', 'POST'])
-@roles_required('admin')
-def school_all_list():
-    schools = School.query.filter_by().all()
-    return render_template('admin/school_list.html', schools=schools, current_schools_only=False)
-
-
-@admin.route('school/<school_id>', methods=['GET', 'POST'])
-@roles_required('admin')
-def school_info(school_id):
-    current_school = School.query.filter_by(id=school_id).first()
-    if current_school:
-        return render_template('admin/school_info.html', school=current_school)
-    else:
-        flash("School with id " + str(school_id) + " did not find", "danger")
-        return redirect(url_for('admin.school_list'))
-
-
-@admin.route('school/add', methods=['GET', 'POST'])
-@roles_required('admin')
-def school_add():
-    form = SchoolForm()
-    if form.validate_on_submit():
-        new_school = School()
-        form.populate_obj(new_school)
-        # save new school to db
-        db.session.add(new_school)
-        db.session.commit()
-        flash(new_school.name + " created", "success")
-        return redirect(url_for('admin.school_list'))
-    else:
-        return render_template('admin/school_add_edit.html', form=form, action='add')
-
-
-@admin.route('school/<school_id>/edit', methods=['GET', 'POST'])
-@roles_required('admin')
-def school_edit(school_id):
-    current_school = School.query.filter_by(id=school_id).first()
-    form = SchoolForm()
-    if form.validate_on_submit():
-        form.populate_obj(current_school)
-        # save to db
-        db.session.commit()
-        flash(current_school.name + " edited", "success")
-        return redirect(url_for('admin.school_list'))
-    else:
-        if current_school:
-            form = SchoolForm(obj=current_school)
-            return render_template('admin/school_add_edit.html', form=form, action='edit', school=current_school)
-        else:
-            flash("School with id " + str(school_id) + " did not find", "danger")
-            return redirect(url_for('admin.school_list'))
 
 
 @admin.route('/teacher', methods=['GET', 'POST'])
@@ -111,14 +42,6 @@ def event_list():
     events = Event.query.filter_by().all()
     # TODO current_events_only - by date or Schdl_Class.current = True. if today in range(start_date_of_class, end_date_of_class)
     return render_template('admin/event_list.html', events=events, current_events_only=False)
-
-
-@admin.route('/user', methods=['GET', 'POST'])
-@roles_required('admin')
-def user_list():
-    # users = Role.query.filter_by(name='admin').first().users  # admins only
-    users = User.query.all()
-    return render_template('admin/user_list.html', users=users)
 
 
 @admin.route('/user/<user_id>/edit', methods=['GET', 'POST'])
