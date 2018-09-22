f5c33756d6e2243b5a94075d38b1fc2f00b066db
k <gavrilovks@gmail.com>
2018-09-21 21:14:53 -0500

    teacher

--- app/teacher/forms.py
+++ app/teacher/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Optional
 
 
 class TeacherForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
+    note = StringField('Note', validators=[Optional()])
     current = BooleanField('Current Teacher')
     # password = StringField('Password', validators=[DataRequired()])
     #email = StringField('Email', validators=[DataRequired()])
--- app/teacher/views.py
+++ app/teacher/views.py
@@ -1,37 +1,29 @@
-from flask import Flask, request, render_template, session, Blueprint, flash, redirect, url_for
+from flask import render_template, Blueprint, redirect, url_for, flash
+from flask_security import roles_required
+
 from app import db
 from app.models import Teacher
 from .forms import TeacherForm
-
 
 teacher = Blueprint('teacher', __name__, template_folder='templates')
 
 
 @teacher.route('/', methods=['GET', 'POST'])
-def main():
+@roles_required('admin')
+def teacher_list():
     teachers = Teacher.query.filter_by(current=True).all()
-    return render_template('teacher/teacher_list.html', teachers=teachers, current_teachers_only=True)
+    return render_template('teacher/list.html', teachers=teachers, current_teachers_only=True)
 
 
 @teacher.route('/all', methods=['GET', 'POST'])
-def teacher_list():
+def teacher_all_list():
     teachers = Teacher.query.all()
-    return render_template('teacher/teacher_list.html', teachers=teachers, current_teachers_only=False)
-
-
-@teacher.route('/<teacher_id>', methods=['GET', 'POST'])
-def info(teacher_id):
-    current_teacher = Teacher.query.filter_by(id=teacher_id).first()
-    if current_teacher:
-        return render_template('teacher/teacher_info.html', teacher=current_teacher)
-    else:
-        flash("Teacher with id " + str(teacher_id) + " did not find", "danger")
-        return redirect(url_for('teacher.main'))
-
+    return render_template('teacher/list.html', teachers=teachers, current_teachers_only=False)
 
 
 @teacher.route('/add', methods=['GET', 'POST'])
-def add_teacher():
+@roles_required('admin')
+def teacher_add():
     form = TeacherForm()
     if form.validate_on_submit():
         new_teacher = Teacher()
@@ -40,14 +32,24 @@ def add_teacher():
         db.session.add(new_teacher)
         db.session.commit()
         flash(new_teacher.first_name + " " + new_teacher.last_name + " created", "success")
-        return redirect(url_for('teacher.main'))
+        return redirect(url_for('teacher.teacher_list'))
     else:
-        return render_template('teacher/add.html', form=form)
+        return render_template('teacher/add_edit.html', form=form, action='add')
 
+
+@teacher.route('/<teacher_id>', methods=['GET', 'POST'])
+@roles_required('admin')
+def info(teacher_id):
+    current_teacher = Teacher.query.filter_by(id=teacher_id).first()
+    if current_teacher:
+        return render_template('teacher/info.html', teacher=current_teacher)
+    else:
+        flash("Teacher with id " + str(teacher_id) + " did not find", "danger")
+        return redirect(url_for('teacher.teacher_list'))
 
 
 @teacher.route('/edit/<teacher_id>', methods=['GET', 'POST'])
-def edit_teacher(teacher_id):
+def teacher_edit(teacher_id):
     current_teacher = Teacher.query.filter_by(id=teacher_id).first()
     form = TeacherForm()
     if form.validate_on_submit():
@@ -55,11 +57,11 @@ def edit_teacher(teacher_id):
         #save to db
         db.session.commit()
         flash(current_teacher.first_name + " " + current_teacher.last_name + " edited", "success")
-        return redirect(url_for('teacher.main'))
+        return redirect(url_for('teacher.teacher_list'))
     else:
         if current_teacher:
             form = TeacherForm(obj=current_teacher)
-            return render_template('teacher/edit.html', form=form, teacher_id=teacher_id)
+            return render_template('teacher/add_edit.html', form=form, teacher_id=teacher_id)
         else:
             flash("Teacher with id " + str(teacher_id) + " did not find", "danger")
-            return redirect(url_for('teacher.main'))
+            return redirect(url_for('teacher.teacher_list'))
