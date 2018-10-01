d95e36d05d88c7857e891cd197e3381525763023
k <gavrilovks@gmail.com>
2018-10-01 00:47:19 -0500

    price and info of class. summernote html editor added

--- app/schdl_class/forms.py
+++ app/schdl_class/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, RadioField, BooleanField
+from wtforms import SubmitField, SelectField, DecimalField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Optional
 
 
@@ -7,7 +7,8 @@ class ClassForm(FlaskForm):
     school_id = SelectField('School', coerce=int, validators=[Optional()])
     teacher_id = SelectField('Teacher', coerce=int, validators=[Optional()])
     subject_id = SelectField('Subject', coerce=int, validators=[Optional()])
-    #note = StringField('Description', validators=[DataRequired()])
+    price = DecimalField('Price', validators=[DataRequired()])
+    info = TextAreaField('Class Description')
     current = BooleanField('Current Class')
     submit = SubmitField('Update')
 
