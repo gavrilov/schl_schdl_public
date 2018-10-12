1a2c9f72b1db6676381268604c668bc548620a07
k <gavrilovks@gmail.com>
2018-10-12 13:25:53 -0500

    add validators = Optional

--- app/schdl_class/forms.py
+++ app/schdl_class/forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, SelectField, DecimalField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, Optional
+from wtforms.validators import Optional
 
 
 class ClassForm(FlaskForm):
     school_id = SelectField('School', coerce=int, validators=[Optional()])
     teacher_id = SelectField('Teacher', coerce=int, validators=[Optional()])
     subject_id = SelectField('Subject', coerce=int, validators=[Optional()])
-    price = DecimalField('Price', validators=[DataRequired()])
-    info = TextAreaField('Class Description')
-    current = BooleanField('Current Class')
+    price = DecimalField('Price', validators=[Optional()])
+    info = TextAreaField('Class Description', validators=[Optional()])
+    current = BooleanField('Current Class', validators=[Optional()])
     submit = SubmitField('Update')
 
