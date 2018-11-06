a14a9239feb44bfe6488697b92a625dcf2858590
k <gavrilovks@gmail.com>
2018-11-06 09:41:44 -0600

    student gender

--- app/student/forms.py
+++ app/student/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, InputRequired, Optional
 
@@ -7,8 +7,8 @@ from wtforms.validators import DataRequired, InputRequired, Optional
 class StudentForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired("Please enter First Name of Student")])
     last_name = StringField('Last Name', validators=[DataRequired("Please enter your Last Name of Student")])
-    gender = BooleanField('Boy',
-                          validators=[Optional("Please select Gender of Student")])  # True = boy, False = girl
+    gender = SelectField('Gender', choices=[('1', 'Boy'), ('2', 'Girl')],
+                         validators=[Optional("Please select Gender of Student")])
     dob = DateField('Date of Birth', validators=[InputRequired("Please enter Day of Birth")])
     # current = BooleanField('Current Teacher')
     submit = SubmitField('Submit')
