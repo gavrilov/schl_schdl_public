c4569e3b8f25eea4e62d9033fa8fe02c850885f9
K <@>
2018-11-26 15:35:30 -0600

    add type of school

--- app/school/forms.py
+++ app/school/forms.py
@@ -1,12 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, SelectField
-from wtforms.validators import DataRequired, Optional
+from wtforms.validators import DataRequired, Optional, ValidationError
+
+
+def selection_validator(form, field):
+    if field.data == 0 or field.data == '0':
+        raise ValidationError("You have not selected type of school")
 
 
 class SchoolForm(FlaskForm):
     name = StringField('School Name', validators=[DataRequired()])
     director_name = StringField('Director Name', validators=[Optional()])
     note = StringField('Note', validators=[Optional()])
+    type = SelectField('Type',
+                        choices=[(0, '---'), (1, 'PP/CC'), (2, 'PP/EC')],
+                        validators=[selection_validator])
     current = BooleanField('Current School')
     # password = StringField('Password', validators=[DataRequired()])
     # email = StringField('Email', validators=[DataRequired()])
