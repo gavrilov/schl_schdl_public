6f3d81c2978c177d37c1d193f7e1af141a262a2c
K <@>
2018-11-26 15:49:41 -0600

    type of school validator

--- app/school/forms.py
+++ app/school/forms.py
@@ -1,11 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Optional, ValidationError
+from flask import flash
 
 
 def selection_validator(form, field):
     if field.data == 0 or field.data == '0':
-        raise ValidationError("You have not selected type of school")
+        flash('You have not selected {} '.format(field.label.text), 'danger')
+        raise ValidationError('You have not selected {} '.format(field.label.text))
 
 
 class SchoolForm(FlaskForm):
@@ -13,7 +15,7 @@ class SchoolForm(FlaskForm):
     director_name = StringField('Director Name', validators=[Optional()])
     note = StringField('Note', validators=[Optional()])
     type = SelectField('Type',
-                        choices=[(0, '---'), (1, 'PP/CC'), (2, 'PP/EC')],
+                        choices=[('0', '---'), ('PP/CC', 'PP/CC'), ('PP/EC', 'PP/EC')],
                         validators=[selection_validator])
     current = BooleanField('Current School')
     # password = StringField('Password', validators=[DataRequired()])
