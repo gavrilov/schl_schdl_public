b35291b875ca2e65bb4a9e728d6ead3d9c6aa42f
K <@>
2018-11-28 16:30:43 -0600

    add card and pay def

--- app/payment/views.py
+++ app/payment/views.py
@@ -31,6 +31,28 @@ def update_card():
     return redirect(redirect_url())
 
 
+@payment.route('/update_card_and_pay', methods=['POST'])
+@login_required
+def update_card_and_pay():
+    # Update Card and Pay in one click. This def for special button on class enrollment page
+    stripe.api_key = current_app.config['STRIPE_SECRET_KEY']
+    token = request.form['stripeToken']
+    class_id = request.form['class_id']
+    student_id = request.form['student_id']
+    if not current_user.stripe_id:
+        # Create a Stripe Customer:
+        customer = stripe.Customer.create(source=token, email=current_user.email)
+        current_user.stripe_id = customer.id
+        db.session.commit()
+    else:
+        # Update a Card of Customer
+        customer = stripe.Customer.retrieve(current_user.stripe_id)
+        customer.source = token
+        customer.save()
+    flash('Your card has been added', 'success')
+    return redirect(url_for('schdl_class.payment_class', class_id=class_id, student_id=student_id))
+
+
 def redirect_url(default='user.main'):
     return request.args.get('next') or \
            request.referrer or \
