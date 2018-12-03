df15e4ce188dbdb3839c374d3bc09c3de8effe11
k <gavrilovks@gmail.com>
2018-12-02 21:32:24 -0600

    teacher dashboard added

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,8 +1,8 @@
-from flask import render_template, Blueprint
-from flask_security import roles_required
+from flask import render_template, Blueprint, flash, redirect
+from flask_security import roles_required, current_user
 
 from app import db
-from app.models import User, Student
+from app.models import User, Student, Teacher
 
 dashboard = Blueprint('dashboard', __name__, template_folder='templates')
 
@@ -13,3 +13,13 @@ def main():
     users = db.session.query(User).count()
     students = db.session.query(Student).count()
     return render_template('dashboard/dashboard.html', users=users, students=students)
+
+
+@dashboard.route('/teacher', methods=['GET', 'POST'])
+@roles_required('teacher')
+def teacher_dashboard():
+    teacher = Teacher.query.filter_by(user_id=current_user.id).first()
+    if not teacher or not teacher.current:
+        flash('Teacher does not find. Please contact administration')
+        return redirect('hello_world')
+    return render_template('dashboard/dashboard_teacher.html', teacher=teacher)
