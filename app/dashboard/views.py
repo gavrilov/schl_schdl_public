04dfd9f68fd25a88789ce4cd6e56fcdbd95ee1ce
k <gavrilovks@gmail.com>
2018-12-06 09:54:34 -0600

    refactor(admin): Reformat code

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,7 +1,6 @@
 from flask import render_template, Blueprint, flash, redirect, url_for
 from flask_security import roles_required, current_user
 
-from app import db
 from app.models import User, Student, Teacher, Enrollment
 
 dashboard = Blueprint('dashboard', __name__, template_folder='templates')
@@ -11,10 +10,10 @@ dashboard = Blueprint('dashboard', __name__, template_folder='templates')
 @roles_required('admin')
 def main():
     # Dashboard for Admins
-    users = db.session.query(User).count()
-    students = db.session.query(Student).count()
-    enrollments_num = db.session.query(Enrollment).count()
-    five_last_enrollments = db.session.query(Enrollment).order_by(Enrollment.id.desc()).limit(5).all()
+    users = User.query.count()
+    students = Student.query.count()
+    enrollments_num = Enrollment.query.count()
+    five_last_enrollments = Enrollment.query.order_by(Enrollment.id.desc()).limit(5).all()
     return render_template('dashboard/dashboard.html', users=users, students=students, enrollments_num=enrollments_num,
                            five_last_enrollments=five_last_enrollments)
 
