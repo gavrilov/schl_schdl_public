b10a857d921e42ca19b223337169d56a65dedb31
k <gavrilovks@gmail.com>
2018-12-10 16:33:13 -0600

    feat(babel): test locale

--- app/__init__.py
+++ app/__init__.py
@@ -179,6 +179,7 @@ def create_app(config_class=Config):
 
     @babel.localeselector
     def get_locale():
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
+        # return request.accept_languages.best_match(app.config['LANGUAGES'])
+        return 'ru'  # to test specific language of babel
 
     return app
