5071b2976b984db3eb6b08fd8eca565e629c6fd4
konst <gavrilovks@gmail.com>
2019-01-30 16:57:44 -0600

    feat(admin): Create page to add student of user

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,11 +1,13 @@
 from flask import render_template, Blueprint, flash, redirect, url_for, request
 from flask_babelex import _
 from flask_security import roles_required, current_user
+from datetime import datetime
 
 from app import db
 from app.dashboard.forms import AddContactForm
-from app.models import User, Student, Teacher, Enrollment
-from app.models import UserContacts
+from app.student.forms import StudentForm
+from app.models import User, Student, Teacher, Enrollment, School, UserContacts
+
 
 dashboard = Blueprint('dashboard', __name__, template_folder='templates')
 
@@ -60,3 +62,34 @@ def add_contact_information():
             flash(error, 'danger')
 
     return render_template('dashboard/add_contact_info.html', form=form, user_id=user_id)  # step=1 for progress bar
+
+
+
+@dashboard.route('user/<user_id>/student/add/', methods=['GET', 'POST'])
+@roles_required('admin')
+def add_student(user_id):
+    current_schools = School.query.filter_by(current=True, hide_from_users=False).order_by(School.name.asc()).all()
+    # Now forming the list of tuples for SelectField
+    school_list = [(i.id, i.name) for i in current_schools]
+    this_user = User.query.filter_by(id=user_id).first()
+    if not this_user:
+        flash(_('User did not find'), 'danger')
+        return redirect(url_for('user.user_list'))
+
+    form = StudentForm()
+    form.default_school_id.choices = [(0, "---")]+school_list
+    if form.validate_on_submit():
+        new_student = Student()
+        form.dob.data = datetime.strptime('{}/{}/{}'.format(form.dob_month.data, form.dob_day.data, form.dob_year.data),
+                                          '%m/%d/%Y')
+        form.populate_obj(new_student)
+        this_user.students.append(new_student)
+        db.session.commit()
+        flash(_('Student has been created'), 'success')
+        return redirect(url_for('student.info', student_id=new_student.id))
+
+    if form.errors:
+        for error in form.errors.values():
+            flash(error, 'danger')
+
+    return render_template('dashboard/student_add.html', form=form, user=this_user)
