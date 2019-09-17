9208b483d1de7fd49ab946edec8800e2d0bb6986
konst <gavrilovks@gmail.com>
2019-09-17 07:56:00 -0700

    feat(admin, user): add 2018, 2019, 2020 year to student dob

--- app/student/forms.py
+++ app/student/forms.py
@@ -39,7 +39,8 @@ class StudentForm(FlaskForm):
     dob_year = SelectField(_l('Year'), choices=[('0', _l('Year')), ('2004', '2004'), ('2005', '2005'), ('2006', '2006'),
                                                 ('2007', '2007'), ('2008', '2008'), ('2009', '2009'), ('2010', '2010'),
                                                 ('2011', '2011'), ('2012', '2012'), ('2013', '2013'), ('2014', '2014'),
-                                                ('2015', '2015'), ('2016', '2016'), ('2017', '2017')],
+                                                ('2015', '2015'), ('2016', '2016'), ('2017', '2017'), ('2018', '2018'),
+                                                ('2019', '2019'), ('2020', '2020')],
                            validators=[selection_validator])
     default_school_id = SelectField(_l('School'), coerce=int, validators=[selection_validator])
     grade = SelectField(_l('Grade'), choices=[(0, '---'), (-3, _l('Preschool')), (-2, _l('Pre-Kindergarten')),
